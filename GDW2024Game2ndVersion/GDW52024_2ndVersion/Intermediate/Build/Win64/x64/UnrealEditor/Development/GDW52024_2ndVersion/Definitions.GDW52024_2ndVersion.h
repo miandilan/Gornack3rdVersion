da43eef767558e80e84fe5be25dade4e3cfd8830
khang.bui@ontariotechu.net <92752927+khangbui28@users.noreply.github.com>
2024-03-23 11:32:53 -0400

    Player Fix

--- GDW2024Game2ndVersion/GDW52024_2ndVersion/Intermediate/Build/Win64/x64/UnrealEditor/Development/GDW52024_2ndVersion/Definitions.GDW52024_2ndVersion.h
+++ GDW2024Game2ndVersion/GDW52024_2ndVersion/Intermediate/Build/Win64/x64/UnrealEditor/Development/GDW52024_2ndVersion/Definitions.GDW52024_2ndVersion.h
@@ -1,4 +1,4 @@
-#include "C:/Users/Ethan/Documents/Unreal Projects/Gornack3rdVersion/GDW2024Game2ndVersion/GDW52024_2ndVersion/Intermediate/Build/Win64/x64/GDW52024_2ndVersionEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "C:/Users/M/Documents/University/Gornack3rdVersion/GDW2024Game2ndVersion/GDW52024_2ndVersion/Intermediate/Build/Win64/x64/GDW52024_2ndVersionEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
